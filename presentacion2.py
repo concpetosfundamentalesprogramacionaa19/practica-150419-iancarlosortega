a4bbe62374f5a4cf104b491dfc71cec22a72c6ac
Ian <iancarlosortegaleon@gmail.com>
2019-04-15 16:49:41 -0500

    ejercicios python

---
+++ presentacion2.py
@@ -0,0 +1,16 @@
+"""
+	Este es mi tercer ejemplo
+	en lenguaje python
+
+	Ingreso de informacion por teclado
+
+	@iancarlosortega
+
+"""
+
+nombre = input ("Por favor ingrese su nombre: \n")
+
+apellido = input ("Por favor ingrese su apellido: \n")
+
+print("Mi nombre es: %s \n\tMi apellido es: \n%s" % (nombre,apellido))
+
